1ea72758444aef75e373fe1c55ef900a33a0ad53
JB-Tellez <jb.tellez@gmail.com>
2017-05-06 14:49:18 -0700

    added multiple rovers, hud display

--- rover.js
+++ rover.js
@@ -1,25 +1,173 @@
-var myRover = {
-  position: [0,0], 
-  direction: 'N'
-};
+
+const READY = 'READY';
+const WAITING = 'WAITING';
+const DONE = 'DONE';
+
+const rovers = [
+  {
+    position: [0, 0],
+    heading: 'N',
+    name: 'roverOne',
+    commands: ['f', 'f', 'f', 'f', 'f', 'r', 'f', 'f'],
+    commandIndex: 0,
+    status: READY
+  },
+  {
+    position: [9, 9],
+    heading: 'S',
+    name: 'roverTwo',
+    commands: ['f', 'f', 'f', 'f', 'f', 'r', 'f', 'f'],
+    commandIndex: 0,
+    status: READY
+  }
+
+];
+
+
+
+const obstacles = [
+  [3, 0],
+  [9, 7]
+];
+
+// order matters
+// presumes starting at north and moving clockwise like a compass
+const headings = ['N', 'E', 'S', 'W'];
+
+const WIDTH = 10; // the number of rows in the grid. Since grid is a square WIDTH = HEIGHT
+
+
 
 function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
+
+  let nextPosition = [rover.position[0], rover.position[1]];
+
+  const movementInfo = {
+    'N': [0, 1],
+    'E': [1, 1],
+    'S': [0, WIDTH - 1],
+    'W': [1, WIDTH - 1]
   };
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  let posIndex = movementInfo[rover.heading][0];
+  let addend = movementInfo[rover.heading][1];
+
+  nextPosition[posIndex] = (nextPosition[posIndex] + addend) % WIDTH;
+
+  obstacles.forEach(obstacle => {
+    if (obstacle[0] === nextPosition[0] && obstacle[1] === nextPosition[1]) {
+      rover.status = DONE;
+      console.log('will hit rock');
+    }
+  });
+
+  if (rover.status !== DONE) {
+
+    rover.position = nextPosition;
+  }
+
 }
 
-goForward(myRover);
+function getHeading(curHeading, turn) {
+
+
+
+  let headingIndex = headings.indexOf(curHeading);
+  let headingCount = headings.length;
+
+  switch (turn) {
+
+    case 'r':
+
+      headingIndex = (headingIndex + 1) % headingCount;
+
+      break;
+
+    case 'l':
+
+      headingIndex = (headingIndex + headingCount - 1) % headingCount;
+
+      break;
+
+    case 'b':
+
+      headingIndex = (headingIndex + headingCount / 2) % headingCount;
+
+      break;
+  }
+
+  return headings[headingIndex];
+}
+
+function obeyCommand(cmd, rover) {
+
+  switch (cmd) {
+
+    case 'f':
+
+      goForward(rover);
+
+      break;
+
+    case 'b':
+
+      rover.heading = getHeading(rover.heading, cmd);
+
+      goForward(rover);
+
+      break;
+
+    case 'r':
+    case 'l':
+
+      rover.heading = getHeading(rover.heading, cmd);
+
+      break;
+  }
+
+}
+
+
+function startMission(rovers) {
+
+  var hud = document.getElementById("hud");
+
+  while (rovers[0].status !== DONE || rovers[1].status !== DONE) {
+
+    for (let r = 0; r < rovers.length; r++) {
+
+      let rover = rovers[r];
+
+      if (rover.status !== DONE) {
+        let command = rover.commands[rover.commandIndex];
+
+        hud.innerHTML = hud.innerHTML + '<br>' + rover.name + ' current position [' + rover.position + '] heading ' + rover.heading;
+
+        obeyCommand(command, rover);
+
+        if (rover.commandIndex < rover.commands.length - 1) {
+          rover.commandIndex = rover.commandIndex + 1;
+        } else {
+          rover.status = DONE;
+        }
+
+        hud.innerHTML = hud.innerHTML + ' --- ' + command + ' [' + rover.position + '] ';
+
+        if (rover.status === DONE) {
+          hud.innerHTML = hud.innerHTML + ' all done!!!';
+        }
+
+      }
+
+    }
+  }
+
+
+
+}
+
+function start() {
+  startMission(rovers);
+}
+
+window.onload = start;
